f20822dc812b9907440bdcd7faf96ec585f695c2
Chris Klanac <cklanac@users.noreply.github.com>
2018-05-07 12:56:49 -0400

    Fix DB reference

--- config.js
+++ config.js
@@ -2,6 +2,6 @@
 
 exports.PORT = process.env.PORT || 8080;
 
-exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful-app';
+exports.MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/noteful';
 
-exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-app-test';
+exports.TEST_MONGODB_URI = process.env.TEST_MONGODB_URI || 'mongodb://localhost/noteful-test';
