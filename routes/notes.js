6e3b90b10be10f42c0b3f8417df879ef024d09d6
Brandon Graham <bgraham452@gmail.com>
2018-07-20 11:21:26 -0400

    Solved error with invalid tag Ids returning 500 err instead of 400. All tests are passing.

--- routes/notes.js
+++ routes/notes.js
@@ -36,6 +36,16 @@ function validateTagIds(tags, userId) {
     err.status = 400;
     return Promise.reject(err);
   }
+
+  const badIds = tags.filter(tag => !mongoose.Types.ObjectId.isValid(tag));
+
+  if (badIds.length){
+    const err = new Error('The `tags` array contains an invalid `id`');
+    err.status = 400;
+    return Promise.reject(err);  
+  }
+
+
   return Tag.find({ $and: [{ _id: { $in: tags }, userId }] })
     .then(results => {
       if (tags.length !== results.length) {
